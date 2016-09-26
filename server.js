d6b42ddc9d8be38873632071feeb4fb8fcdd4707
Dion <Dion.Fulwood@gmail.com>
2016-09-26 03:02:31 -0700

    Completes simple express server.

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+var express = require('express');
+var path = require('path');
+var app = express();
 
 var jsonData = {count: 12, message: 'hey'};
+
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/data', function(req, res) {
+  res.send(jsonData);
+});
+
+app.listen(3000, function() {
+  console.log("Server running! 🌎 🌍 🌏")
+});
